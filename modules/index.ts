56af55019b22bf76d1ce30263e124cd87c26708f
sultondev <sultondeveloper@gmail.com>
2023-05-29 03:09:18 +0500

    fix: translate

---
+++ modules/index.ts
@@ -0,0 +1,29 @@
+import { createResolver, defineNuxtModule } from '@nuxt/kit'
+
+export default defineNuxtModule({
+    async setup(options, nuxt) {
+        const { resolve } = createResolver(import.meta.url)
+
+        // @ts-ignore
+        nuxt.hook('i18n:registerModule', (register: any) => {
+            register({
+                // langDir path needs to be resolved
+                langDir: resolve('../lang'),
+                locales: [
+                    {
+                        code: 'en',
+                        file: 'en.json',
+                    },
+                    {
+                        code: 'ru',
+                        file: 'ru.json',
+                    },
+                    {
+                        code: 'uz',
+                        file: 'uz.json',
+                    },
+                ]
+            })
+        })
+    }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import TranslateModule from './modules/index'
 export default defineNuxtConfig({
     modules: [
+        TranslateModule,
         '@nuxtjs/tailwindcss',
         '@nuxtjs/color-mode',
         '@pinia/nuxt',
@@ -16,5 +18,24 @@ export default defineNuxtConfig({
         exposeConfig: false,
         injectPosition: 0,
         viewer: true,
+    },
+    i18n: {
+        lazy: false,
+        langDir: 'lang',
+        locales: [
+            {
+                code: 'en',
+                file: 'en.json'
+            },
+            {
+                code: 'ru',
+                file: 'ru.json'
+            },
+            {
+                code: 'uz',
+                file: 'uz.json'
+            }
+        ],
+        defaultLocale: 'en'
     },
 })
