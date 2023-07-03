10d3f5916b3c3c80cf9cc42445b0f144d1919101
git config --global user.name sultondevgit config --global user.name <sultondeveloper@gmail.com>
2023-07-03 15:53:21 +0500

    fix: added twitter protocol

--- app.vue
+++ app.vue
@@ -12,7 +12,14 @@ useServerSeoMeta({
   ogDescription: metaOgDescription.value,
   ogImage: 'https://media.licdn.com/dms/image/D4D22AQEPLzt9l-pOXg/feedshare-shrink_2048_1536/0/1688331795181?e=1691020800&v=beta&t=sZrEm1xBU0H16SY3zC7JS9TmDx3nLy0vEYzMUJ-sIQw',
   'og:image:height': '1000',
-  keywords: `Full-Stack Developer, Front-End Development, Back-End Development, Web Applications, User Experience (UX), Responsive Design, Creative Designs, Robust Functionalities, Immersive Experiences, Innovation, Quality, Attention to Detail, Digital Presence, Website Development, Portfolio Showcase, Technology Integration, Code Optimization, Problem-Solving, Collaboration, Client Satisfaction`
+  keywords: `Full-Stack Developer, Front-End Development, Back-End Development, Web Applications, User Experience (UX), Responsive Design, Creative Designs, Robust Functionalities, Immersive Experiences, Innovation, Quality, Attention to Detail, Digital Presence, Website Development, Portfolio Showcase, Technology Integration, Code Optimization, Problem-Solving, Collaboration, Client Satisfaction`,
+  ogType: 'website',
+  twitterCard: 'summary_large_image',
+  // twitterDomain: '',
+  // twitterUrl: '',
+  twitterTitle: metaOgTitle.value,
+  twitterDescription: description.value,
+  twitterImage: 'https://media.licdn.com/dms/image/D4D22AQEPLzt9l-pOXg/feedshare-shrink_2048_1536/0/1688331795181?e=1691020800&v=beta&t=sZrEm1xBU0H16SY3zC7JS9TmDx3nLy0vEYzMUJ-sIQw'
 })
 
 
