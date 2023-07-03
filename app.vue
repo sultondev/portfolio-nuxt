a737c22fb1cec4d24c0e0072b45895e63eca8f13
git config --global user.name sultondevgit config --global user.name <sultondeveloper@gmail.com>
2023-07-03 10:16:28 +0500

    feat: added page loading indicator

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtLoadingIndicator color="#54b689" height="3" throttle="0" duration="4000" />
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
